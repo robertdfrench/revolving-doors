63dcab78c4f56a8262cc34b09c89b352e2e4d15c
Robert D French <robert@robertdfrench.me>
2019-03-19 00:06:14 -0400

    Add a client that counts door_call invocations

--- Makefile
+++ Makefile
@@ -1,2 +1,10 @@
-server:
+test: client server
+	./server &
+	sleep 1
+	time ./client
+
+server: server.c
 	cc server.c -o $@
+
+client: client.c
+	cc client.c -o $@
---
+++ client.c
@@ -0,0 +1,37 @@
+#include <door.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+	char* path = "server.door";
+        int door = open(path, O_RDONLY);
+        if (door == -1) {
+                perror("Could not open door");
+                exit(1);
+        }
+
+        char request[12];
+        char reply[12];
+
+        door_arg_t args;
+        args.data_ptr = request;
+        args.data_size = 12;
+        args.desc_ptr = NULL;
+        args.desc_num = 0;
+        args.rbuf = reply;
+        args.rsize = 12;
+
+	for(int i = 0; i < 1000000; i++) {
+		int result = door_call(door, &args);
+		if (result == -1) {
+			perror("Could not call door");
+			exit(1);
+		}
+	}
+        printf(args.rbuf);
+        printf("\n");
+        return 0;
+}
