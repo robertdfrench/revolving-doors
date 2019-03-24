dba519a92cdc50b8944d558f140ac0116bab78f9
Robert D French <robert@robertdfrench.me>
2019-03-23 20:38:27 -0400

    Factor out help into lib.mk

--- Makefile
+++ Makefile
@@ -1,4 +1,13 @@
-test:
-	$(MAKE) -C hello_world test
-	$(MAKE) -C door_through_door test
-	$(MAKE) -C payload_size test
+include lib.mk
+
+test: $(subst /,,*/) ## Run all tests
+	$(info done!)
+
+hello_world: host_check ## Open a door an receive a greeting
+	$(MAKE) -C $@ test
+
+door_through_door: host_check ## Open a door on a client's behalf
+	$(MAKE) -C $@ test
+
+payload_size: host_check ## How much can we send and how fast can we send it?
+	$(MAKE) -C $@ test
---
+++ lib.mk
@@ -0,0 +1,15 @@
+.PHONY: help
+
+help:
+	@# Help Menu due to https://marmelab.com/blog/2016/02/29/auto-documented-makefile.html
+	@grep -Eh '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) \
+		| sort \
+		| awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+host_check: illumos cc;
+
+illumos:
+	@[ `uname` = SunOS ] || (echo "You need to be on Illumos to run these tests."; exit 1)
+
+cc:
+	@(which cc) || (echo "You need to install a C compiler to run these tests."; exit 1)
