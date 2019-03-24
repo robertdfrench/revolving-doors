5e0edd95f600dc04a91a03416c9b5d17be555929
Robert D French <robert@robertdfrench.me>
2019-03-24 17:53:25 -0400

    Elaborate on how we create the door path idempotently

--- 10_filesystem_background/README.md
+++ 10_filesystem_background/README.md
@@ -1,2 +1,11 @@
 # Preparing to hang the door
+Because doors have a filesystem representation, we need to make sure they have a
+nice, clean place to live on the filesytem. This means creating a new, empty
+file and marking it with the appropriate permissions. To do that, we make use of
+the following system calls:
 
+* [STAT(2)](https://www.illumos.org/man/2/stat) checks if a path exists
+* [CREAT(2)](https://www.illumos.org/man/2/creat) make new file, set permissions
+
+Run `make test` to see this lesson in action. Notice how we can run `./server`
+once to create the file, and then run it again without producing an error.
--- 10_filesystem_background/server.c
+++ 10_filesystem_background/server.c
@@ -5,20 +5,21 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 
+// Create the file 'server.door' if it does not already exist
 int main() {
         struct stat buf;
 	char* path = "server.door";
 
+	// Check to see if a file exists at 'path'
         if (stat(path, &buf) < 0) {
                 int newfd;
+
+		// No such file exists, so lets create it
                 if ((newfd = creat(path, 0444)) < 0) {
-                        perror("creat");
+                        perror("Could not create a new file for the door");
                         exit(1);
                 }
-                close(newfd);
         }
-
-        fdetach(path);
 
 	return 0;
 }
