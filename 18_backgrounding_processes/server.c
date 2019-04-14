5171dcee28441f01633c0d03e6d00ded6ecf5ac1
Robert D French <robert@robertdfrench.me>
2019-04-13 22:32:44 -0400

    Remove all but `sleep` from 18/server. Fixes #13

--- 18_backgrounding_processes/server.c
+++ 18_backgrounding_processes/server.c
@@ -1,31 +1,19 @@
-#include <door.h>
-#include <stdio.h>
-#include <unistd.h>
+#include <err.h>
 #include <fcntl.h>
 #include <sys/stat.h>
-#include <err.h>
-
-void answer(void* cookie, char* args, size_t nargs, door_desc_t* descriptors, uint_t ndescriptors) {
-	printf("Someone knocked on my door!\n");
-	door_return(NULL, 0, NULL, 0);
-}
+// New headers for this lesson
+#include <unistd.h>
+#include <stdio.h>
 
 int main() {
-	struct stat buf;
-
 	char* path = "server.door";
 
-	int door = door_create(&answer, NULL, 0);
-	if (door == -1) err(1, "Handle cannot be attached to door");
-
+	// Attempt to create a file at 'path'
 	int fd = open(path, O_RDWR|O_CREAT|O_EXCL, 0400);
 	if (fd < 0) err(1, "Could not create a new file for the door");
 
-	int attachment = fattach(door, path);
-	if (attachment == -1) err(1, "Could not attach door to server.door");
-
-	printf("%s will remain attached to this process for 1 hour\n", path);
-
+	// Leave this server running until `make test` is done with it.
+	printf("Waiting in the background...\n");
 	sleep(3600);
 
 	return 0;
