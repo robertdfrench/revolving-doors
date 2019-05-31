dc2381e0c0bd428d323db75a80890ffa341f4176
Robert D French <robert@robertdfrench.me>
2019-05-31 08:12:56 -0400

    Use door_return for main thread after lesson 98

--- E0_door_through_door/server.c
+++ E0_door_through_door/server.c
@@ -23,9 +23,5 @@ int main() {
 	int attachment = fattach(door, path);
 	if (attachment == -1) err(1, "Could not attach door to server.door");
 
-	printf("%s will remain attached to this process for 1 hour\n", path);
-
-	sleep(3600);
-
-	return 0;
+	return door_return(NULL, 0, NULL, 0);
 }
